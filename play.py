7465a153d86665f777431225c8f13442a89f529f
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2024-11-04 10:20:45 +0100

    init

---
+++ play.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+    # The first subgoal is to explore all numbers from 1 to k in all possible ways in which it can be done.
+    # That's actually just all orderings, so k!, k faculty, implying it grows astronomically.
+    # For example, if k is 100 that is already 3 628 800, rounded to 4 million.
+    # Ok that's too many possibilities. I'll change the task to writing python functions
+    # for ways of structured counting that I can think of, hopefully fun ways :) .
+    # We are not interested in different programming language techniques of counting,
+    # but rather in the mathematical structures of counting.
+    # Each way shall be representend in a separate generator function.
+    # The functions shall be tested with the same input, to see if they give the same result, i.e. the numbers from 1 to k, after sorting the output.
+    # Each generator function invocation takes a number k and yields the numbers between 1 and k in a certain order.
+
+def 
+
+
